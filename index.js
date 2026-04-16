2a7209e67019aba3ae8f5eac96be6e37c688ee29
ToneBarbell <kurtdekortes@gmail.com>
2026-04-16 17:21:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const manifest = {
-    id: "org.tvitaly.live",
-    version: "1.0.0",
-    name: "Ita Live",
-    resources: ["catalog", "stream"],
-    types: ["tv"],
-    catalogs: [{ type: "tv", id: "italy_channels", name: "TV Italiana" }]
-};
-
-const builder = new addonBuilder(manifest);
-
-builder.defineCatalogHandler(() => {
-    return Promise.resolve({
-        metas: [{
-            id: "it_rai1",
-            type: "tv",
-            name: "Rai 1",
-            poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Rai_1_logo_%282016%29.svg/200px-Rai_1_logo_%282016%29.svg.png"
-        }]
-    });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Content-Type", "application/json");
+    next();
 });
 
-builder.defineStreamHandler((args) => {
-    if (args.id === "it_rai1") {
-        return Promise.resolve({
-            streams: [{ 
-                title: "Diretta HD", 
-                url: "https://direttarai1.akamaized.net/hls/live/2017001/rai1/playlist.m3u8" 
-            }]
-        });
-    }
-    return Promise.resolve({ streams: [] });
+app.get("/manifest.json", (req, res) => {
+    res.sendFile(path.join(__dirname, "manifest.json"));
 });
 
-const addonInterface = builder.getInterface();
-serveHTTP(addonInterface, { port: process.env.PORT || 10000 });
+app.listen(process.env.PORT || 3000);
