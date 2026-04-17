288c3b53c5ae5fa9787b92e36014e198bd4ff997
ToneBarbell <kurtdekortes@gmail.com>
2026-04-17 11:37:54 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,47 +10,47 @@ function sendJson(res, data) {
 
 const manifest = {
   id: "org.zapprtv.geremia",
-  version: "1.0.3",
+  version: "1.0.5",
   name: "Zappr Geremia",
-  description: "Canali TV nazionali e locali Lombardia",
+  description: "Canali TV da Zappr",
   resources: ["catalog", "stream"],
   types: ["tv"],
   catalogs: [
     {
       type: "tv",
       id: "zappr_tv",
-      name: "Zappr TV Lombardia"
+      name: "Zappr TV"
     }
   ]
 };
 
 const channels = [
-  { id: "zappr_rai1", type: "tv", name: "Rai 1", stream: "https://rai.zappr.stream/rai1.m3u8" },
-  { id: "zappr_rai2", type: "tv", name: "Rai 2", stream: "https://rai.zappr.stream/rai2.m3u8" },
-  { id: "zappr_rai3", type: "tv", name: "Rai 3", stream: "https://rai.zappr.stream/rai3.m3u8" },
-  { id: "zappr_rete4", type: "tv", name: "Rete 4", stream: "https://mediaset.zappr.stream/rete4.m3u8" },
-  { id: "zappr_canale5", type: "tv", name: "Canale 5", stream: "https://mediaset.zappr.stream/canale5.m3u8" },
-  { id: "zappr_italia1", type: "tv", name: "Italia 1", stream: "https://mediaset.zappr.stream/italia1.m3u8" },
-  { id: "zappr_la7", type: "tv", name: "La7", stream: "https://la7.zappr.stream/la7.m3u8" },
-  { id: "zappr_tv8", type: "tv", name: "TV8", stream: "https://sky.zappr.stream/tv8.m3u8" },
-  { id: "zappr_nove", type: "tv", name: "Nove", stream: "https://discovery.zappr.stream/nove.m3u8" },
-
-  { id: "zappr_rai4", type: "tv", name: "Rai 4", stream: "https://rai.zappr.stream/rai4.m3u8" },
-  { id: "zappr_rai5", type: "tv", name: "Rai 5", stream: "https://rai.zappr.stream/rai5.m3u8" },
-  { id: "zappr_raimovie", type: "tv", name: "Rai Movie", stream: "https://rai.zappr.stream/raimovie.m3u8" },
-  { id: "zappr_raipremium", type: "tv", name: "Rai Premium", stream: "https://rai.zappr.stream/raipremium.m3u8" },
-  { id: "zappr_rainews24", type: "tv", name: "Rai News 24", stream: "https://rai.zappr.stream/rainews24.m3u8" },
-  { id: "zappr_raisport", type: "tv", name: "Rai Sport", stream: "https://rai.zappr.stream/raisport.m3u8" },
-  { id: "zappr_raiyoyo", type: "tv", name: "Rai Yoyo", stream: "https://rai.zappr.stream/raiyoyo.m3u8" },
-  { id: "zappr_raigulp", type: "tv", name: "Rai Gulp", stream: "https://rai.zappr.stream/raigulp.m3u8" },
-
-  { id: "zappr_bergamotv", type: "tv", name: "Bergamo TV", stream: "https://lombardia.zappr.stream/bergamotv.m3u8" },
-  { id: "zappr_milanopaviatv", type: "tv", name: "Milano Pavia TV", stream: "https://lombardia.zappr.stream/milanopaviatv.m3u8" },
-  { id: "zappr_seilatvbergamo", type: "tv", name: "Seilatv Bergamo", stream: "https://lombardia.zappr.stream/seilatvbergamo.m3u8" },
-  { id: "zappr_teletutto", type: "tv", name: "Teletutto", stream: "https://lombardia.zappr.stream/teletutto.m3u8" },
-  { id: "zappr_telelombardia", type: "tv", name: "Telelombardia", stream: "https://lombardia.zappr.stream/telelombardia.m3u8" },
-  { id: "zappr_antenna3", type: "tv", name: "Antenna 3", stream: "https://lombardia.zappr.stream/antenna3.m3u8" },
-  { id: "zappr_telenova", type: "tv", name: "Telenova", stream: "https://lombardia.zappr.stream/telenova.m3u8" }
+  {
+    id: "zappr_rai1",
+    type: "tv",
+    name: "Rai 1",
+    stream: "https://rai.zappr.stream/rai1.m3u8"
+  },
+  {
+    id: "zappr_rai2",
+    type: "tv",
+    name: "Rai 2",
+    stream: "https://rai.zappr.stream/rai2.m3u8"
+  },
+  {
+    id: "zappr_rai3",
+    type: "tv",
+    name: "Rai 3",
+    stream: "https://rai.zappr.stream/rai3.m3u8"
+  },
+  {
+    id: "zappr_topcrime",
+    type: "tv",
+    name: "Top Crime",
+    poster: "https://channels.zappr.stream/logos/topcrime.svg",
+    background: "https://channels.zappr.stream/logos/topcrime.svg",
+    stream: "https://live02-col.msf.cdn.mediaset.net/live/ch-lt/lt-clr.isml/index.m3u8"
+  }
 ];
 
 app.get("/manifest.json", (req, res) => {
@@ -63,6 +63,8 @@ app.get("/catalog/tv/zappr_tv.json", (req, res) => {
       id: channel.id,
       type: channel.type,
       name: channel.name,
+      poster: channel.poster,
+      background: channel.background,
       posterShape: "square"
     }))
   });
