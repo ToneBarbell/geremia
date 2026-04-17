1c89ab239d58f10c5586fbba9e33005306b8e51d
ToneBarbell <kurtdekortes@gmail.com>
2026-04-17 11:24:31 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,9 +10,9 @@ function sendJson(res, data) {
 
 const manifest = {
   id: "org.zapprtv.geremia",
-  version: "1.0.0",
+  version: "1.0.1",
   name: "Zappr Geremia",
-  description: "Addon test",
+  description: "Canali TV da Zappr",
   resources: ["catalog", "stream"],
   types: ["tv"],
   catalogs: [
@@ -24,28 +24,58 @@ const manifest = {
   ]
 };
 
+const channels = [
+  {
+    id: "zappr_rai1",
+    type: "tv",
+    name: "Rai 1",
+    poster: "https://raw.githubusercontent.com/ZapprTV/channels/main/it/logos/rai1.svg",
+    stream: "https://rai.zappr.stream/rai1.m3u8"
+  },
+  {
+    id: "zappr_rai2",
+    type: "tv",
+    name: "Rai 2",
+    poster: "https://raw.githubusercontent.com/ZapprTV/channels/main/it/logos/rai2.svg",
+    stream: "https://rai.zappr.stream/rai2.m3u8"
+  },
+  {
+    id: "zappr_rai3",
+    type: "tv",
+    name: "Rai 3",
+    poster: "https://raw.githubusercontent.com/ZapprTV/channels/main/it/logos/rai3.svg",
+    stream: "https://rai.zappr.stream/rai3.m3u8"
+  }
+];
+
 app.get("/manifest.json", (req, res) => {
   sendJson(res, manifest);
 });
 
 app.get("/catalog/tv/zappr_tv.json", (req, res) => {
   sendJson(res, {
-    metas: [
-      {
-        id: "zappr_rai1",
-        type: "tv",
-        name: "Rai 1"
-      }
-    ]
+    metas: channels.map((channel) => ({
+      id: channel.id,
+      type: channel.type,
+      name: channel.name,
+      poster: channel.poster,
+      posterShape: "square"
+    }))
   });
 });
 
-app.get("/stream/tv/zappr_rai1.json", (req, res) => {
+app.get("/stream/tv/:id.json", (req, res) => {
+  const channel = channels.find((c) => c.id === req.params.id);
+
+  if (!channel) {
+    return sendJson(res, { streams: [] });
+  }
+
   sendJson(res, {
     streams: [
       {
-        title: "Rai 1",
-        url: "https://rai.zappr.stream/rai1.m3u8"
+        title: channel.name,
+        url: channel.stream
       }
     ]
   });
